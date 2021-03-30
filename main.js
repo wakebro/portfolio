702cf7396190030c6703fe3eab39fb316094eac2
wake <wakebro119@naver.com>
2021-03-30 13:37:04 +0900

    JS : navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+// const location = document.querySelector("#home").offsetTop;
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) return;
+  console.log(event.target.dataset.link);
+  const scrollTarget = document.getElementById(link);
+  scrollTarget.scrollIntoView({ behavior: "smooth" });
+});

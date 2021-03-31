c5b3f2b8c89f4e236e74025a3d3a91c475389d5e
wake <wakebro119@naver.com>
2021-03-31 11:17:33 +0900

    JS : navbar toggle, project ui

--- main.js
+++ main.js
@@ -17,7 +17,14 @@ navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) return;
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", (event) => {
+  navbarMenu.classList.toggle("open");
 });
 
 // Handle scrolling when tapping on the contact button of home

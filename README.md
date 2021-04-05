fb0722d637e02b8fedeed7bd3e935d024a062d11
wake <wakebro119@naver.com>
2021-04-05 12:38:36 +0900

    write comment

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # portfolio
- Ali's Portfolio
+
+Ali's Portfolio
+
+HTML, CSS, JavaScript로 만든 포트폴리오 웹 사이트 구현
+
+##특징
+
+- 자기 소개
+- 사용 기술
+- 프로젝트
+- 연락처
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // Make navbar transparent when it is on the top
+// 상단 메뉴 생성
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -12,6 +13,7 @@ document.addEventListener("scroll", () => {
 });
 
 // Handle scrolling when tapping on the navbar menu
+// 상단 메뉴 선택 시 자동 스크롤
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
@@ -22,18 +24,21 @@ navbarMenu.addEventListener("click", (event) => {
 });
 
 // Navbar toggle button for small screen
+// 화면 작아질 시 토글버튼 생성
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
 navbarToggleBtn.addEventListener("click", (event) => {
   navbarMenu.classList.toggle("open");
 });
 
 // Handle scrolling when tapping on the contact button of home
+// Home 섹션 Contact 버튼 클릭 시 Contact 섹션으로 이동
 const contactBtn = document.querySelector(".home__contact");
 contactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
 // Make home slowly fade to transparent as the window scrolls down
+// 스크롤 내릴 시 Home 섹션 흐려지기
 const home = document.querySelector(".home__container");
 const homeheight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -41,6 +46,7 @@ document.addEventListener("scroll", () => {
 });
 
 // Make arrow to move the top
+// 상단 바로가기 버튼
 const arrow = document.querySelector("#arrowBtn");
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
@@ -52,6 +58,7 @@ arrow.addEventListener("click", (event) => {
 });
 
 // Work filter
+// 프로젝트 필터
 const workBtnContainer = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
